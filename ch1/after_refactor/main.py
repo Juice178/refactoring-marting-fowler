77e52c1bb1c1c3095de2d6787ad990bf91135c97
Kenta <kenasuzuki5555@gmail.com>
2021-03-10 10:47:51 +0900

    Move functions inside statement function

--- ch1/after_refactor/main.py
+++ ch1/after_refactor/main.py
@@ -4,6 +4,50 @@ import locale
 
 
 def statement(invoice, plays):
+    def total_amount():
+        result = 0
+        for perf in invoice['performances']:
+            result += amount_for(perf)
+        return result
+
+    def amount_for(a_performance):
+        result = 0
+        if play_for(a_performance)['type'] == 'tragedy':
+            result = 40000
+            if a_performance['audience'] > 30:
+                result += 1000 * (a_performance["audience"] - 30)
+        
+        elif play_for(a_performance)['type'] == 'comedy':
+            result = 30000
+            if a_performance['audience'] > 20:
+                result += 10000 + 500 * (a_performance["audience"] - 20)
+            result += 300 * a_performance["audience"]
+        else:
+            raise Exception(f"unkownn type: {play_for(a_performance)['type']}")
+
+        return result
+
+    def play_for(a_performance):
+        return plays[a_performance['playID']]
+
+    def volume_credits_for(a_performance):
+        result  = 0
+        result += max(a_performance['audience'] - 30, 0)
+        if "comedy" == play_for(a_performance)['type']:
+            result += math.floor(a_performance['audience']/ 5)
+        return result
+
+    def usd(a_number):
+        locale.setlocale(locale.LC_ALL, 'en_US.utf-8')
+        return locale.format_string('%.2f', a_number / 100, True)
+
+    def total_volume_credits():
+        result = 0
+        for perf in invoice['performances']:
+            result += volume_credits_for(perf)
+        return result
+
+
     result = f"Statement for {invoice['customer']}\n"
     for perf in invoice['performances']:
         result += f"  { play_for(perf)['name']}: {usd(amount_for(perf))} seats)\n"
@@ -13,53 +57,6 @@ def statement(invoice, plays):
     result += f"Amount owed is {usd(total_amount())}\n"
     result += f"You earned {total_volume_credits()} credits\n"
     return result
-
-
-def amount_for(a_performance):
-    result = 0
-    if play_for(a_performance)['type'] == 'tragedy':
-        result = 40000
-        if a_performance['audience'] > 30:
-            result += 1000 * (a_performance["audience"] - 30)
-    
-    elif play_for(a_performance)['type'] == 'comedy':
-        result = 30000
-        if a_performance['audience'] > 20:
-            result += 10000 + 500 * (a_performance["audience"] - 20)
-        result += 300 * a_performance["audience"]
-    else:
-        raise Exception(f"unkownn type: {play_for(a_performance)['type']}")
-
-    return result
-    
-
-def play_for(a_performance):
-    return plays[a_performance['playID']]
-
-def volume_credits_for(a_performance):
-    result  = 0
-    result += max(a_performance['audience'] - 30, 0)
-    if "comedy" == play_for(a_performance)['type']:
-        result += math.floor(a_performance['audience']/ 5)
-    return result
-
-def usd(a_number):
-    locale.setlocale(locale.LC_ALL, 'en_US.utf-8')
-    return locale.format_string('%.2f', a_number / 100, True)
-
-def total_volume_credits():
-    result = 0
-    for perf in invoice['performances']:
-        result += volume_credits_for(perf)
-    return result
-
-
-def total_amount():
-    result = 0
-    for perf in invoice['performances']:
-        result += amount_for(perf)
-    return result
-
 
 if __name__ == "__main__":
     with open('invoices.json') as f:
