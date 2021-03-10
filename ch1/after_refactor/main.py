674a2f9e83b1992d6452ccc8be533ed8aa59a08c
Kenta <kenasuzuki5555@gmail.com>
2021-03-10 21:26:56 +0900

    Add functions to render data to HTML

--- ch1/after_refactor/main.py
+++ ch1/after_refactor/main.py
@@ -9,20 +9,34 @@ def statement(invoice, plays):
     return render_plain_text(create_statement_data(invoice, plays))
 
 def render_plain_text(data):
-    def usd(a_number):
-        locale.setlocale(locale.LC_ALL, 'en_US.utf-8')
-        return locale.currency(a_number / 100)
-
     result = f"Statement for {data['customer']}\n"
-    # print(data)
     for perf in data['performances']:
         result += f"  { perf['play']['name']}: {usd(perf['amount'])} ( {perf['audience']} seats)\n"
-
-    # total_amount = apple_sauce()
 
     result += f"Amount owed is {usd(data['total_amount'])}\n"
     result += f"You earned {data['total_volume_credits']} credits\n"
     return result
+
+
+def html_statement(invoice, plays):
+    return render_html(create_statement_data(invoice, plays))
+
+
+def render_html(data):
+    result = f"<h1>Statement for {data['customer']}</h1>\n"
+    result += "<table>\n"
+    result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>"
+    for perf in data['performances']:
+        result += f"<tr><td>{perf['play']['name']}</td><td>{perf['audience']}</td>"
+        result += f"<td>{usd(perf['amount'])}</td></tr>\n"
+    result += "</table>\n"
+    result += f"<p>Amount owed is <em>{usd(data['total_amount'])}</em></p>\n"
+    result += f"<p>You earned <em>{data['total_volume_credits']}</em> credits</p>\n"
+    return result
+
+def usd(a_number):
+    locale.setlocale(locale.LC_ALL, 'en_US.utf-8')
+    return locale.currency(a_number / 100)
 
 
 if __name__ == "__main__":
@@ -32,4 +46,7 @@ if __name__ == "__main__":
         plays = json.load(f)
     invoice = invoices[0]
     result = statement(invoice, plays)
+    print(result)
+
+    result = html_statement(invoice, plays)
     print(result)
