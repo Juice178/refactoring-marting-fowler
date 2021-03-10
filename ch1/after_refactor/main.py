b0bf07c6726250db8070de12ac9c68552a27488b
Kenta <kenasuzuki5555@gmail.com>
2021-03-10 14:56:36 +0900

    Put play_for function inside statement function

--- ch1/after_refactor/main.py
+++ ch1/after_refactor/main.py
@@ -6,7 +6,11 @@ import locale
 def statement(invoice, plays):
     def enrich_performance(a_performance):
         result = a_performance.copy()
+        result['play'] = play_for(result)
         return result
+
+    def play_for(a_performance):
+        return plays[a_performance['playID']]
 
     statement_data = {}
     statement_data['customer'] = invoice['customer']
@@ -15,36 +19,34 @@ def statement(invoice, plays):
     return render_plain_text(statement_data, plays)
 
 def render_plain_text(data, plays):
+    # print(data)
     def total_amount():
         result = 0
-        for perf in invoice['performances']:
+        for perf in data['performances']:
             result += amount_for(perf)
         return result
 
     def amount_for(a_performance):
         result = 0
-        if play_for(a_performance)['type'] == 'tragedy':
+        if a_performance['play']['type'] == 'tragedy':
             result = 40000
             if a_performance['audience'] > 30:
                 result += 1000 * (a_performance["audience"] - 30)
         
-        elif play_for(a_performance)['type'] == 'comedy':
+        elif a_performance['play']['type'] == 'comedy':
             result = 30000
             if a_performance['audience'] > 20:
                 result += 10000 + 500 * (a_performance["audience"] - 20)
             result += 300 * a_performance["audience"]
         else:
-            raise Exception(f"unkownn type: {play_for(a_performance)['type']}")
+            raise Exception(f"unkownn type: {a_performance['type']}")
 
         return result
-
-    def play_for(a_performance):
-        return plays[a_performance['playID']]
 
     def volume_credits_for(a_performance):
         result  = 0
         result += max(a_performance['audience'] - 30, 0)
-        if "comedy" == play_for(a_performance)['type']:
+        if "comedy" == a_performance['play']['type']:
             result += math.floor(a_performance['audience']/ 5)
         return result
 
@@ -54,14 +56,15 @@ def render_plain_text(data, plays):
 
     def total_volume_credits():
         result = 0
-        for perf in invoice['performances']:
+        for perf in data['performances']:
             result += volume_credits_for(perf)
         return result
 
 
     result = f"Statement for {data['customer']}\n"
+    # print(data)
     for perf in data['performances']:
-        result += f"  { play_for(perf)['name']}: {usd(amount_for(perf))} ( {perf['audience']} seats)\n"
+        result += f"  { perf['play']['name']}: {usd(amount_for(perf))} ( {perf['audience']} seats)\n"
 
     # total_amount = apple_sauce()
 
