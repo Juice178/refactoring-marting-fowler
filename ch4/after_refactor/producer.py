4cb76d21963b3f234cea44c54e8ad71206f940ba
Kenta <kenasuzuki5555@gmail.com>
2021-03-16 23:00:01 +0900

    Add non-refactored code

---
+++ ch4/after_refactor/producer.py
@@ -0,0 +1,29 @@
+class Producer:
+    def __init__(self, aProvince, data) -> None:
+        self._province = aProvince
+        self._cost = data['cost']
+        self._name = data['name']
+        self._production = data['production'] or 0
+    
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def cost(self):
+        return self._cost
+
+    @cost.setter
+    def cost(self, arg):
+        self._cost = int(arg)
+
+    @property
+    def production(self):
+        return self._production
+
+    @production.setter
+    def production(self, amountStr):
+        amount = int(amountStr)
+        newProduction = 0 if amount is None else amount
+        self._province['totalProduction'] += newProduction - self._production
+        self._production = newProduction
---
+++ ch4/after_refactor/province.py
@@ -0,0 +1,77 @@
+from producer import Producer
+from functools import reduce
+
+class Province:
+    def __init__(self, doc) -> None:
+        self._name = doc['name']
+        self._producers = []
+        self._totalProduction = 0
+        self._demand = doc['demand']
+        self._price = doc['price']
+        for d in doc['producers']:
+            self.addProducer(Producer(self, d))
+
+    def addProducer(self, arg) -> None:
+        self._producers.append(arg)
+        self._totalProduction += arg.production
+        return self._totalProduction
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def producers(self):
+        return self._producers[:]
+    
+    @property
+    def totalProduction(self):
+        return self._totalProduction
+
+    @totalProduction.setter
+    def totalProduction(self, arg):
+        self._totalProduction = arg
+
+    @property
+    def demand(self):
+        return self._demand
+
+    @demand.setter
+    def demand(self, arg):
+        self._demand = int(arg)
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, arg):
+        self._price = int(arg)
+
+    @property
+    def shortFall(self):
+        return self._demand - self.totalProduction
+
+    @property
+    def profit(self):
+        return self.demandValue - self.demandCost
+
+    @property
+    def demandCost(self):
+        remainingDemand = self.demand
+        result = 0
+        self.prodcers = [p.cost for p in self.producers].sort()
+        for p in self.producers:
+            constribution = min(remainingDemand, p.production)
+            remainingDemand -= constribution
+            result += constribution * p.cost
+        return result
+
+
+    @property
+    def demandValue(self):
+        return self.satisfiedDemand * self.price
+
+    @property
+    def satisfiedDemand(self):
+        return min(self._demand, self.totalProduction)
