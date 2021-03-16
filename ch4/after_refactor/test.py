8d2ac12aebee7dce31d2f7175f75a646b64341b7
Kenta <kenasuzuki5555@gmail.com>
2021-03-17 02:09:13 +0900

    Add test for Province class

--- ch4/after_refactor/test.py
+++ ch4/after_refactor/test.py
@@ -1,6 +1,18 @@
 from province import Province
 from data import sampleProvinceData
+import pytest
 
-def test_shortfall():
-    asia = Province(sampleProvinceData())
-    assert asia.shortFall == 5
+
+@pytest.fixture()
+def asia():
+    return Province(sampleProvinceData())
+
+@pytest.mark.usefixtures('asia')
+class TestProvince:
+    def test_shortfall(self, asia):
+        #asia = Province(sampleProvinceData())
+        assert asia.shortFall == 5
+
+    def test_province(self, asia):
+        #asia = Province(sampleProvinceData())
+        assert asia.profit == 230
